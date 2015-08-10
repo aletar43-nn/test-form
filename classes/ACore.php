fed49ed6a778087bee348c13b5fadea0a903fa5e
Alexey Tarambula <aletar43-nn@yandex.ru>
2015-08-10 16:23:57 +0400

    Абстрактный класс ядро

--- classes/ACore.php
+++ classes/ACore.php
@@ -1,4 +1,22 @@
 <?php
+abstract class  ACore {
 
+    protected $db;
+
+    public function __construct(){
+        $this->db = mysql_connect(HOST, USER, PASSWORD);
+        if(!$this->db){
+            exit('Ошибка соединения с базой данных '.mysql_error());
+        }
+        if(!mysql_select_db(DB,$this->db)){
+            exit('Нет такой базы данных! '.mysql_error());
+        }
+        mysql_query('SET NAMES "UTF8"');
+    }
+
+    public function get_body(){
+        echo 'Hello World!';
+    }
+}
 
 ?>
